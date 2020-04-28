9e7bd93101eefe79cc97d7578f699b76da9d32cb
Mohamed Ashraf <mohamedashrafma05@gmail.com>
2020-04-28 14:42:49 +0200

    users

--- models/cat.js
+++ models/cat.js
@@ -81,84 +81,7 @@ const validateToggleNeglectCats = (body) => {
     return Joi.validate(body, schema);
 }
 
-async function getProducts(input) {
-    let startId = input.params.id;
-    
-    console.log(startId)
-    let aggr = [
-        {
-          '$match': {
-            '_id': mongoose.Types.ObjectId(startId),
-            'isNeglected': false
-          }
-        },
-        {
-            '$lookup': {
-              'from': 'products', 
-              'localField': '_id', 
-              'foreignField': 'cat', 
-              'as': 'products'
-            }
-        },
-        {
-            '$unwind': {
-              'path': '$products',
-              'preserveNullAndEmptyArrays': true
-            }
-          },
-            {
-            '$lookup': {
-              'from': 'productPrices', 
-              'localField': 'products._id', 
-              'foreignField': 'product', 
-              'as': 'productPrices'
-            }
-        },
-        {
-            '$unwind': {
-              'path': '$productPrices',
-              'preserveNullAndEmptyArrays': true
-            }
-          },
-        {
-            '$addFields': {
-              'products.price': "$productPrices.prices",
-            }
-          },
-            {
-              '$group': {
-               '_id': '$products',
-               'subCategoryImage': {
-                '$first': '$avatar'
-            },
-               }
-           },
-           {
-            '$addFields': {
-              'subCategoryImage': { $concat: [input.app.get('defaultAvatar')(input, 'host'), "$subCategoryImage"] },
-            }
-          },
-           {
-            '$project': {
-                'subCategoryImage': 1,
-                '_id._id': 1,
-                '_id.nameAr': 1,
-                '_id.nameEn': 1,
-                '_id.avatar': 1,
-                '_id.price.initialPrice': 1,
-                '_id.price.reducedPrice': 1,
-                '_id.newPrice': { "$subtract": ['$_id.price.initialPrice',{"$multiply": [ { "$divide": ["$_id.price.reducedPrice",100] }, '$_id.price.initialPrice' ]}]},
-            }
-           }, 
-           
-      ];
-      let getProducts = await Cat.aggregate(aggr);
-      getProducts = getProducts.map(product => {
-        product._id.avatar = product._id.avatar.map(avatar=>{avatar= input.app.get('defaultAvatar')(input, 'host') + avatar ;return avatar })
-        return product;
-    })
-      return (getProducts);
-  }
+
 const getCats_OldWithoutHasChildren = async (input) => {
 
     let { startId = false, limit = 10, all = false } = input.query;
@@ -516,6 +439,5 @@ module.exports = {
     updateCat,
     toggleNeglectCats,
     getsubCategories,
-    getProducts
 }
 
