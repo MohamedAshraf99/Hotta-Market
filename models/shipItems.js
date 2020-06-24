02c563f469b5ceb00b334609a90f602ef9f5f13c
unknown <mohamedashrafma05@gmail.com>
2020-06-24 13:00:59 +0200

    bestReviews

--- models/shipItems.js
+++ models/shipItems.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
-
-
+const { Product } = require('../models/product');
+const { AppSettings } = require('./appSettings')
 const shipItemsSchema = new mongoose.Schema({
     orderShips: {
         type: mongoose.Schema.Types.ObjectId,
@@ -89,7 +89,158 @@ const validateAddShipItems = (body) => {
 
 async function getBestSelling(input) {
     let userId = input.query.userId;
-
+    let appSettings = await AppSettings.findOne();
+    let maxLimit = appSettings.maxLimit;
+    let productMethod = appSettings.productMethod;
+    if(productMethod == "manual")
+    {
+      let agg = [
+        {
+          '$match': {
+            'isNeglected': false,
+            'available': true,
+            'bestReviews': true
+          }
+        },
+            {
+            '$lookup': {
+              'from': 'productprices', 
+              'localField': '_id', 
+              'foreignField': 'product', 
+              'as': 'ProductPrices'
+            }
+        },
+        {
+            '$unwind': {
+              'path': '$ProductPrices',
+              'preserveNullAndEmptyArrays': true
+            }
+          },
+          {
+            '$lookup': {
+              'from': 'favouriteproducts', 
+              'localField': '_id', 
+              'foreignField': 'product', 
+              'as': 'favouriteProducts'
+            }
+        },
+                {
+            '$unwind': {
+              'path': '$favouriteProducts',
+              'preserveNullAndEmptyArrays': true
+            }
+          },
+          {
+            '$lookup': {
+              'from': 'shipcards', 
+              'localField': 'ProductPrices._id', 
+              'foreignField': 'productPrice', 
+              'as': 'shipCards'
+            }
+        },
+                {
+            '$unwind': {
+              'path': '$shipCards',
+              'preserveNullAndEmptyArrays': true
+            }
+          },
+          {
+            '$lookup': {
+              'from': 'shipitems', 
+              'localField': 'ProductPrices._id', 
+              'foreignField': 'product.productPrice', 
+              'as': 'shipitems'
+            }
+        },
+                {
+            '$unwind': {
+              'path': '$shipitems',
+              'preserveNullAndEmptyArrays': true
+            }
+          },
+          {
+            '$addFields': {
+                'favourite': {
+                    '$and': [ { '$eq': [ "$favouriteProducts.user", mongoose.Types.ObjectId(userId) ] }, { '$eq': [ "$favouriteProducts.product", "$_id" ] } ]
+                }
+            }
+        },
+        {
+            '$addFields': {
+                'cart': {
+                    '$and': [ { '$eq': [ "$shipCards.client", mongoose.Types.ObjectId(userId) ] }, { '$eq': [ "$ProductPrices._id", "$shipCards.productPrice" ] } ]
+                },
+                'productPrices':'$ProductPrices._id'
+            }
+        },
+            {
+              '$group': {
+              '_id': '$_id',
+              'count': { '$sum': 1 },
+              'avatar': {
+              '$first': '$avatar'
+              },
+              'nameAr': {
+                '$first': '$nameAr'
+              },
+              'nameEn': {
+                '$first': '$nameEn'
+              },
+              'productPrices': {
+                  '$first': '$productPrices'
+              },
+              'bestReviews': {
+                '$first': '$bestReviews'
+              },
+              'available': {
+                '$first': '$available'
+              },
+              'favourite': {
+                '$first': '$favourite'
+              },
+              'cart': {
+                '$first': '$cart'
+              },
+              'price': {
+                '$first': '$ProductPrices.price'
+              },
+               }
+           },
+           {
+            '$project': {
+                '_id': 1,
+                'avatar': 1,
+                'productPrices': 1,
+                'nameAr': 1,
+                'nameEn': 1,
+                'bestReviews': 1,
+                'available': 1,
+                'price.initialPrice': 1,
+                'price.reducedPrice': { "$ifNull": [ "$price.reducedPrice", "$price.initialPrice" ] },
+                'favourite': 1,
+                'cart': 1,
+                'count': 1,
+            }
+           }, 
+           {
+            '$addFields': {
+              'discountPrecentage': { "$multiply": [100,{"$divide": [ { "$subtract": ["$price.initialPrice","$price.reducedPrice"] }, '$price.initialPrice' ]}]},
+            }
+          },
+          { '$sort' : { 'count' : -1} },
+          { '$limit' : maxLimit }
+       ];
+      let Products = await Product.aggregate(agg);
+      if(Products.length != 0)
+        {
+          Products = Products.map(product=>{
+              product.avatar = input.app.get('defaultAvatar')(input, 'host') + product.avatar;
+              return product = {'_id':product,count:product.count};
+          })
+       }
+      return Products;
+    }
+    else{
     let aggr = [
         {
           '$match': {
@@ -207,7 +358,7 @@ async function getBestSelling(input) {
             }
           },
            { '$sort' : { 'count' : -1} },
-           { '$limit' : 30 }
+           { '$limit' : maxLimit }
       ];
       let getProducts = await shipItems.aggregate(aggr);
       if(getProducts.length != 0)
@@ -218,15 +369,173 @@ async function getBestSelling(input) {
         })
      }
       return (getProducts);
-     
+    }
   }
 
 
   async function getTrending(input) {
     let userId = input.query.userId;
+    let appSettings = await AppSettings.findOne();
+    let maxLimit = appSettings.maxLimit;
+    let productMethod = appSettings.productMethod;
     let currentDate = new Date();
     let lastMonth = new Date();
     lastMonth.setMonth(lastMonth.getMonth() - 1);
+    if(productMethod == "manual")
+    {
+      let agg = [
+        {
+          '$match': {
+            'isNeglected': false,
+            'available': true,
+            'bestReviews': true
+          }
+        },
+            {
+            '$lookup': {
+              'from': 'productprices', 
+              'localField': '_id', 
+              'foreignField': 'product', 
+              'as': 'ProductPrices'
+            }
+        },
+        {
+            '$unwind': {
+              'path': '$ProductPrices',
+              'preserveNullAndEmptyArrays': true
+            }
+          },
+          {
+            '$lookup': {
+              'from': 'favouriteproducts', 
+              'localField': '_id', 
+              'foreignField': 'product', 
+              'as': 'favouriteProducts'
+            }
+        },
+                {
+            '$unwind': {
+              'path': '$favouriteProducts',
+              'preserveNullAndEmptyArrays': true
+            }
+          },
+          {
+            '$lookup': {
+              'from': 'shipcards', 
+              'localField': 'ProductPrices._id', 
+              'foreignField': 'productPrice', 
+              'as': 'shipCards'
+            }
+        },
+                {
+            '$unwind': {
+              'path': '$shipCards',
+              'preserveNullAndEmptyArrays': true
+            }
+          },
+          {
+            '$lookup': {
+              'from': 'shipitems', 
+              'localField': 'ProductPrices._id', 
+              'foreignField': 'product.productPrice', 
+              'as': 'shipitems'
+            }
+        },
+                {
+            '$unwind': {
+              'path': '$shipitems',
+              'preserveNullAndEmptyArrays': true
+            }
+          },
+          {
+            '$match': {
+              'shipitems.isNeglected': false,
+              'shipitems.dateCreate': {'$gte': lastMonth, '$lte': currentDate}
+            }
+          },
+          {
+            '$addFields': {
+                'favourite': {
+                    '$and': [ { '$eq': [ "$favouriteProducts.user", mongoose.Types.ObjectId(userId) ] }, { '$eq': [ "$favouriteProducts.product", "$_id" ] } ]
+                }
+            }
+        },
+        {
+            '$addFields': {
+                'cart': {
+                    '$and': [ { '$eq': [ "$shipCards.client", mongoose.Types.ObjectId(userId) ] }, { '$eq': [ "$ProductPrices._id", "$shipCards.productPrice" ] } ]
+                },
+                'productPrices':'$ProductPrices._id'
+            }
+        },
+            {
+              '$group': {
+              '_id': '$_id',
+              'count': { '$sum': 1 },
+              'avatar': {
+              '$first': '$avatar'
+              },
+              'nameAr': {
+                '$first': '$nameAr'
+              },
+              'nameEn': {
+                '$first': '$nameEn'
+              },
+              'productPrices': {
+                  '$first': '$productPrices'
+              },
+              'bestReviews': {
+                '$first': '$bestReviews'
+              },
+              'available': {
+                '$first': '$available'
+              },
+              'favourite': {
+                '$first': '$favourite'
+              },
+              'cart': {
+                '$first': '$cart'
+              },
+              'price': {
+                '$first': '$ProductPrices.price'
+              },
+               }
+           },
+           {
+            '$project': {
+                '_id': 1,
+                'avatar': 1,
+                'productPrices': 1,
+                'nameAr': 1,
+                'nameEn': 1,
+                'bestReviews': 1,
+                'available': 1,
+                'price.initialPrice': 1,
+                'price.reducedPrice': { "$ifNull": [ "$price.reducedPrice", "$price.initialPrice" ] },
+                'favourite': 1,
+                'cart': 1,
+                'count': 1,
+            }
+           }, 
+           {
+            '$addFields': {
+              'discountPrecentage': { "$multiply": [100,{"$divide": [ { "$subtract": ["$price.initialPrice","$price.reducedPrice"] }, '$price.initialPrice' ]}]},
+            }
+          },
+          { '$sort' : { 'count' : -1} },
+          { '$limit' : maxLimit }
+       ];
+      let Products = await Product.aggregate(agg);
+      if(Products.length != 0)
+        {
+          Products = Products.map(product=>{
+              product.avatar = input.app.get('defaultAvatar')(input, 'host') + product.avatar;
+              return product = {'_id':product,count:product.count};
+          })
+       }
+      return Products;
+    }
+    else{
     let aggr = [
         {
           '$match': {
@@ -345,7 +654,7 @@ async function getBestSelling(input) {
             }
           },
            { '$sort' : { 'count' : -1} },
-           { '$limit' : 30 }
+           { '$limit' : maxLimit }
       ];
       let getProducts = await shipItems.aggregate(aggr);
       if(getProducts.length != 0)
@@ -356,7 +665,7 @@ async function getBestSelling(input) {
         })
      }
       return (getProducts);
-     
+    }
   }
 
 
@@ -366,7 +675,160 @@ async function getBestSelling(input) {
   
 async function getBestReviews(input) {
   let userId = input.query.userId;
-
+  let appSettings = await AppSettings.findOne();
+  let maxLimit = appSettings.maxLimit;
+  let productMethod = appSettings.productMethod;
+  if(productMethod == "manual")
+  {
+  let agg = [
+    {
+      '$match': {
+        'isNeglected': false,
+        'available': true,
+        'bestReviews': true
+      }
+    },
+        {
+        '$lookup': {
+          'from': 'productprices', 
+          'localField': '_id', 
+          'foreignField': 'product', 
+          'as': 'ProductPrices'
+        }
+    },
+    {
+        '$unwind': {
+          'path': '$ProductPrices',
+          'preserveNullAndEmptyArrays': true
+        }
+      },
+      {
+        '$lookup': {
+          'from': 'favouriteproducts', 
+          'localField': '_id', 
+          'foreignField': 'product', 
+          'as': 'favouriteProducts'
+        }
+    },
+            {
+        '$unwind': {
+          'path': '$favouriteProducts',
+          'preserveNullAndEmptyArrays': true
+        }
+      },
+      {
+        '$lookup': {
+          'from': 'shipcards', 
+          'localField': 'ProductPrices._id', 
+          'foreignField': 'productPrice', 
+          'as': 'shipCards'
+        }
+    },
+            {
+        '$unwind': {
+          'path': '$shipCards',
+          'preserveNullAndEmptyArrays': true
+        }
+      },
+      {
+        '$lookup': {
+          'from': 'shipitems', 
+          'localField': 'ProductPrices._id', 
+          'foreignField': 'product.productPrice', 
+          'as': 'shipitems'
+        }
+    },
+            {
+        '$unwind': {
+          'path': '$shipitems',
+          'preserveNullAndEmptyArrays': true
+        }
+      },
+      {
+        '$addFields': {
+            'favourite': {
+                '$and': [ { '$eq': [ "$favouriteProducts.user", mongoose.Types.ObjectId(userId) ] }, { '$eq': [ "$favouriteProducts.product", "$_id" ] } ]
+            }
+        }
+    },
+    {
+        '$addFields': {
+            'cart': {
+                '$and': [ { '$eq': [ "$shipCards.client", mongoose.Types.ObjectId(userId) ] }, { '$eq': [ "$ProductPrices._id", "$shipCards.productPrice" ] } ]
+            },
+            'productPrices':'$ProductPrices._id'
+        }
+    },
+        {
+          '$group': {
+          '_id': '$_id',
+          'rate': {
+            '$avg': "$shipitems.rate.rate" 
+            },
+          'avatar': {
+          '$first': '$avatar'
+          },
+          'nameAr': {
+            '$first': '$nameAr'
+          },
+          'nameEn': {
+            '$first': '$nameEn'
+          },
+          'productPrices': {
+              '$first': '$productPrices'
+          },
+          'bestReviews': {
+            '$first': '$bestReviews'
+          },
+          'available': {
+            '$first': '$available'
+          },
+          'favourite': {
+            '$first': '$favourite'
+          },
+          'cart': {
+            '$first': '$cart'
+          },
+          'price': {
+            '$first': '$ProductPrices.price'
+          },
+           }
+       },
+       {
+        '$project': {
+            '_id': 1,
+            'avatar': 1,
+            'productPrices': 1,
+            'nameAr': 1,
+            'nameEn': 1,
+            'bestReviews': 1,
+            'available': 1,
+            'price.initialPrice': 1,
+            'price.reducedPrice': { "$ifNull": [ "$price.reducedPrice", "$price.initialPrice" ] },
+            'favourite': 1,
+            'cart': 1,
+            'rate': 1,
+        }
+       }, 
+       {
+        '$addFields': {
+          'discountPrecentage': { "$multiply": [100,{"$divide": [ { "$subtract": ["$price.initialPrice","$price.reducedPrice"] }, '$price.initialPrice' ]}]},
+        }
+      },
+      { '$sort' : { 'rate' : -1} },
+      { '$limit' : maxLimit }
+   ];
+  let Products = await Product.aggregate(agg);
+  if(Products.length != 0)
+    {
+      Products = Products.map(product=>{
+          product.avatar = input.app.get('defaultAvatar')(input, 'host') + product.avatar;
+          return product = {'_id':product,rate:product.rate};
+      })
+   }
+  return Products;
+  }
+  else{
   let aggr = [
       {
         '$match': {
@@ -472,6 +934,7 @@ async function getBestReviews(input) {
               '_id.productPrices': 1,
               '_id.nameAr': 1,
               '_id.nameEn': 1,
+              '_id.bestReviews': 1,
               '_id.available': 1,
               '_id.price.initialPrice': 1,
               '_id.price.reducedPrice': { "$ifNull": [ "$_id.price.reducedPrice", "$_id.price.initialPrice" ] },
@@ -485,8 +948,8 @@ async function getBestReviews(input) {
             '_id.discountPrecentage': { "$multiply": [100,{"$divide": [ { "$subtract": ["$_id.price.initialPrice","$_id.price.reducedPrice"] }, '$_id.price.initialPrice' ]}]},
           }
         },
-         { '$sort' : { 'rate' : -1} },
-         { '$limit' : 30 }
+        { '$sort' : { 'rate' : -1} },
+        { '$limit' : maxLimit }
     ];
     let getProducts = await shipItems.aggregate(aggr);
     if(getProducts.length != 0)
@@ -497,7 +960,7 @@ async function getBestReviews(input) {
       })
    }
     return (getProducts);
-   
+  }
 }
 
 
