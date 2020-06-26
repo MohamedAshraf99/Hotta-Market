14d72cb0a38e3073bbe5db462b217d5379441314
ahmedmagdyb7r <ahmedmagdyb7r@gmail.com>
2020-06-26 18:00:10 +0200

    add dev.env to use mongo atlsa

--- startup/logging.js
+++ startup/logging.js
@@ -1,24 +1,23 @@
-const winston = require('winston');
-require('winston-mongodb');
-require('express-async-errors');
+const winston = require("winston");
+require("winston-mongodb");
+require("express-async-errors");
 
-module.exports = function() {
+module.exports = function () {
   winston.handleExceptions(
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
 
-    new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+    new winston.transports.File({ filename: "uncaughtExceptions.log" })
   );
-  
-  process.on('unhandledRejection', (ex) => {
+
+  process.on("unhandledRejection", (ex) => {
     throw ex;
   });
-  
- 
-  winston.add(winston.transports.File, { filename: 'logfile.log' });
+
+  winston.add(winston.transports.File, { filename: "logfile.log" });
   winston.add(winston.transports.MongoDB, {
     // db: 'mongodb://admin2:mrMgr2020@173.212.224.29:27017/mrMgr',
-    db: 'mongodb://admin2:mrMgr2020@hota-project_mongo_1_b66381b3fa08:27017/mrMgr',
+    db: process.env.DB_URL,
     // db: 'mongodb://localhost:27017/mrMgr',
-    level: 'info'
-  });  
-}
+    level: "info",
+  });
+};
