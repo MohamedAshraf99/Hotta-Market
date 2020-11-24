bff39719636dad6ecd9f5333a97fb73654202201
MohamedAshraf99 <30416958+MohamedAshraf99@users.noreply.github.com>
2020-11-24 20:55:20 +0200

    Update db.js

--- startup/db.js
+++ startup/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 module.exports = function () {
   mongoose;
-  // .connect("mongodb://admin2:mrMgr2020@173.212.224.29:27017/mrMgr", {
+  // .connect("mongodb://localhost:27017/mrMgr", {
   //   useCreateIndex: true, // access remotly on server db
   mongoose
     .connect(
-      "mongodb://admin2:mrMgr2020@hota-project_mongo_1_b66381b3fa08:27017/mrMgr",
+      "mongodb://localhost:27017/mrMgr",
       {
         useCreateIndex: true, // must on gitlab when push to server
         // mongoose.connect('mongodb://localhost:27017/mrMgr', {useCreateIndex: true,  // local db
